3b58f40c89633e098f1f2b219867ecd841968cff
aes4096 <ikukhtikov@aes4096.ru>
2025-05-27 14:56:24 +0300

    Create projects

---
+++ apps/blog/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Post, Category
+from django_mptt_admin.admin import DjangoMpttAdmin
+
+@admin.register(Category)
+class CategoryAdmin(DjangoMpttAdmin):
+    """
+    Админ-панель модели категорий
+    """
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post)
