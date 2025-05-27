c475d096ab899e48dcacb45e7304459b26bd24e5
aes4096 <ikukhtikov@aes4096.ru>
2025-05-27 19:25:56 +0300

    Fix .gitignore

--- blog_cbv/urls.py
+++ blog_cbv/urls.py
@@ -21,9 +21,10 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('apps.blog.urls')),
 ]
 
-'''
+
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-'''
